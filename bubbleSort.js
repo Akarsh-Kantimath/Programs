ef960f4a252789de9e6c41aa82bc7722b11cce72
Akarsh Kantimath <akarshkantimath@gmail.com>
2026-01-02 14:57:05 +0530

    bubbleSort program

--- bubbleSort.js
+++ bubbleSort.js
@@ -16,3 +16,24 @@ function bubbleSort(arr) {
 const arr = [-6, 20, 8, -2, 4];
 bubbleSort(arr);
 console.log(arr);
+
+//Practive 1 - 02/01/2026
+// function bubbleSort(arr) {
+//     let flag;
+//     do {
+//         flag = false;
+//         for (let i = 0; i < arr.length - 1; i++) {
+
+//             if (arr[i] > arr[i + 1]) {
+//                 let temp = arr[i];
+//                 arr[i] = arr[i + 1];
+//                 arr[i + 1] = temp;
+//                 flag = true;
+//             }
+//         }
+//     } while (flag);
+// }
+
+// const arr = [5, 17, 8, -5, 4]
+// bubbleSort(arr);
+// console.log(arr);
